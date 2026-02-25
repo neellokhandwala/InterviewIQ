cc1f51326201074b08c14246740e08d7227d529b
neellokhandwala <nvlokhand@gmail.com>
2026-02-25 15:11:58 +0530

    did clerk

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import cors from 'cors';
+import path from 'path';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 
@@ -16,5 +17,23 @@ app.get("/books", (req, res) => {
     res.status(200).json({ msg: "Success Books Check" }); 
 });
 
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+    });
+}
 
-app.listen(ENV.PORT || 10000, () => console.log(`Server running on port ${ENV.PORT || 10000}`));
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log(`Server is running on port ${ENV.PORT}`);
+        });
+    } catch (error) {        
+        console.error(`Failed to start server: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+startServer();
