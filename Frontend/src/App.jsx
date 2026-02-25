253dff619ec027cc36088d8cefee79e945ba7875
neellokhandwala <nvlokhand@gmail.com>
2026-02-25 15:34:19 +0530

    inngest done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
-import { SignInButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton, UserButton } from '@clerk/clerk-react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <h1>Welcome to the app</h1>
-    <SignInButton />
+    <SignedOut>
+      <SignInButton mode="modal">
+        <button className="">
+          Sign In
+        </button>
+      </SignInButton>
+    </SignedOut>
+
+    <SignedIn>
+      <SignOutButton />
+    </SignedIn>
     </>
-  )
+  );
 }
 
 export default App
