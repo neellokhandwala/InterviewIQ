aa105dc2d9115d4d3fab740f738a85c9fc0601d8
neellokhandwala <nvlokhand@gmail.com>
2026-02-27 14:34:47 +0530

    setup tools

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,28 +1,7 @@
 import  { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
 import toast from 'react-hot-toast';
-import { useEffect, useState } from 'react';
 
 function HomePage() {
-    const [books,setBooks] = useState([])
-    const [isLoading,setIsLoading] = useState(true)
-    const [error,setError] = useState(null)
-
-    useEffect(() => {
-        const getBooks = async () => {
-            setIsLoading(true);
-            try {
-                const res = await fetch("/api/books");
-                const data = await res.json();
-                setBooks(data);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setIsLoading(false);            }
-        }
-
-        getBooks();
-    },[]);
-
     return <div>
         <button className="btn btn-secondary" onClick={() => toast.success("This is a success")}>Click me</button>
         <SignedOut>
