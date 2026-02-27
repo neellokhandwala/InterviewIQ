a743b3952e4054f3325e5308e65acab6253663d7
neellokhandwala <nvlokhand@gmail.com>
2026-02-27 15:03:40 +0530

    ..

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,23 +1,8 @@
-import  { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
-import toast from 'react-hot-toast';
-
 function HomePage() {
-    return <div>
-        <button className="btn btn-secondary" onClick={() => toast.success("This is a success")}>Click me</button>
-        <SignedOut>
-            <SignInButton mode="modal">
-                <button>Login</button>
-            </SignInButton>
-        </SignedOut>
-
-        <SignedIn>
-            <SignOutButton/>
-        </SignedIn>
-
-        <UserButton />
-
-    </div>
+    return (
+        <div>
+            
+        </div>
+    );
 }
-
-
 export default HomePage;
